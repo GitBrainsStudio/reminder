b795c15601e7492470909a21b0d9d7622a0908ca
GitBrainsStudio <gitbrains@gmail.com>
2022-05-20 13:21:56 +0500

    fix : imports

--- src/app/pages/reminder-details-page/reminder-details-page.module.ts
+++ src/app/pages/reminder-details-page/reminder-details-page.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReminderDetailsPageComponent } from './reminder-details-page.component';
 import { ReminderDetailsPageRoutingModule } from './reminder-details-page-routing.module';
-import { ReminderModule } from 'src/app/features/reminder/reminder.module';
-import { MaterialModule } from 'src/app/features/material/material.module';
+import { ReminderModule } from '../../features/reminder/reminder.module';
+import { MaterialModule } from '../../features/material/material.module';
 
 @NgModule({
   declarations: [
